8e732f388b91eebe8f9b7bc74f6bbee5865c78dd
Erik Kaufman <141158064+Erik-Kaufman@users.noreply.github.com>
2024-09-05 09:20:29 -0500

    first commit basic beginings

---
+++ TicTacToe.py
@@ -0,0 +1,16 @@
+
+# Goal is to make a tic tac toe game -
+# Erik Kaufman
+
+# TODO: Start with two player mode
+# TODO: Add a bot for to play single player
+# TODO: Expand to select size of board (three in a row, four in a row, as well as board size) 
+
+import numpy as np
+
+class board():
+
+    # initilization
+    def __init__(self):
+        # size = 3
+        board = {[]}
